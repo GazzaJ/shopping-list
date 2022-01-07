0442fff2dec3e1975e8228673a973cf89181f060
Gareth John <gareth.e.john@pwc.com>
2022-01-07 15:02:02 +0000

    Add search functionality and clearing of ad item input

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// ***** Add an Item to the shopping list
+// ***** Add an Item to the shopping list *****
 let addItem = document.getElementById('add-item');
 addItem.addEventListener('submit', (e) => {
   let ul = document.getElementsByTagName('ul')[0];
@@ -6,6 +6,9 @@ addItem.addEventListener('submit', (e) => {
   e.preventDefault();
 
   let newItem = addItem.querySelector('input').value;
+
+  // Clear the input box on Add
+  addItem.querySelector('input').value = null;
 
   // Create list items dynamically
   let li = document.createElement('li');
@@ -27,7 +30,31 @@ addItem.addEventListener('submit', (e) => {
   removeItem.classList.add('delete');
 });
 
-// ***** Delete Items from the shopping list
+// ***** Search Items in the shopping list *****
+const SEARCH = document.forms['item-search'].querySelector('input');
+// Access input value and convert to lowercase
+SEARCH.addEventListener('keyup', (e) => {
+  let input = e.target.value.toLowerCase();
+  // get the list values
+  let shoppingList = document.querySelector('#list ul');
+  let listItems = shoppingList.getElementsByTagName('li');
+  // Convert HTML Collection to an array
+  let itemArray = Array.from(listItems);
+  // loop through values
+  itemArray.forEach((item) => {
+    let itemName = item.firstElementChild.textContent;
+    itemNameLower = itemName.toLowerCase();
+    if (itemNameLower.indexOf(input) == -1) {
+      item.style.display = 'none';
+    } else {
+      item.style.display = 'block';
+    }
+  });
+
+  // match results
+});
+
+// ***** Delete Items from the shopping list *****
 let list = document.getElementById('list');
 list.addEventListener('click', deleteItem);
 
@@ -39,7 +66,7 @@ function deleteItem(e) {
   }
 }
 
-// ***** Hide the shopping list
+// ***** Hide the shopping list *****
 let checkbox = document.getElementById('hide');
 checkbox.addEventListener('change', (e) => {
   let shoppingList = document.getElementById('list');

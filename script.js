3f068299a8f904159fe4ae9544bfe5837b64bf86
Gareth John <gareth.e.john@pwc.com>
2022-01-07 11:32:17 +0000

    Add functionality to hide/show the shopping list

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-// Delete Items from the shopping list
+// ***** Add an Item to the shopping list
+let addItem = document.getElementById('add-item');
+addItem.addEventListener('submit', (e) => {
+  let ul = document.getElementsByTagName('ul')[0];
+  // Prevent the form from refreshing
+  e.preventDefault();
+
+  let newItem = addItem.querySelector('input').value;
+
+  // Create list items dynamically
+  let li = document.createElement('li');
+  let groceryItem = document.createElement('span');
+  let removeItem = document.createElement('span');
+
+  li.appendChild(groceryItem);
+  li.appendChild(removeItem);
+
+  // Add the new Item to the list
+  ul.appendChild(li);
+
+  // Add the item value to the span
+  groceryItem.textContent = newItem;
+  groceryItem.classList.add('item');
+
+  // Add inner html and class to Delete button
+  removeItem.textContent = 'Delete';
+  removeItem.classList.add('delete');
+});
+
+// ***** Delete Items from the shopping list
 let list = document.getElementById('list');
 list.addEventListener('click', deleteItem);
 
@@ -10,4 +39,13 @@ function deleteItem(e) {
   }
 }
 
-// Add an Item to the shopping list
+// ***** Hide the shopping list
+let checkbox = document.getElementById('hide');
+checkbox.addEventListener('change', (e) => {
+  let shoppingList = document.getElementById('list');
+  if (checkbox.checked) {
+    shoppingList.style.display = 'none';
+  } else {
+    shoppingList.style.display = 'block';
+  }
+});
